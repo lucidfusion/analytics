54cda492d9ed5e86b73f9e5ec4edae1da78a6e2e
Danny Campanile <dannyc@lucidfusion.com>
2015-07-09 11:15:32 -0700

    Examples and unminned JS snippet.

---
+++ src/snippet.js
@@ -0,0 +1,9 @@
+var lfa;
+(function(){
+	var s = document.createElement('script');
+	s.setAttribute('src', '//lfa.lucidfusion.com/public/analytics.latest.min.js');
+	s.onload = function(){
+	lfa = new LFA('Xz2d4Tco77p620XZ753PGQRaa5K5QESi');
+	};
+	document.body.appendChild(s);
+})();
